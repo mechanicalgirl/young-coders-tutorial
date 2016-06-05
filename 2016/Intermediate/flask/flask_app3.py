b798d79700807107f95b32772604f8928a77f63e
Taylor J. Meek <Lingnik@users.noreply.github.com>
2016-06-05 13:11:57 -0700

    Create flask_app3.py

---
+++ 2016/Intermediate/flask/flask_app3.py
@@ -0,0 +1,26 @@
+from flask import Flask, redirect, render_template, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="barbaras",
+    password="???",
+    hostname="barbaras.mysql.pythonanywhere-services.com",
+    databasename="barbaras$myflasksample",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
+
+comments = []
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
